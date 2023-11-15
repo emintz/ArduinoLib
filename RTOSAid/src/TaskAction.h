776c3030ab28ec9b637a050ab120a5a3bfe37988
Eric Mintz <16582299+emintz@users.noreply.github.com>
2023-11-15 17:50:19 -0500

    Update TaskAction.h
    Fix garbled file.

--- RTOSAid/src/TaskAction.h
+++ RTOSAid/src/TaskAction.h
@@ -1,37 +1,118 @@
 /*
- * Action.cpp
+ * Action.h
  *
  *  Created on: May 9, 2023
  *      Author: Eric Mintz
+ *
+ * Base class for providing logic, a.k.a the "task loop", that runs within
+ * a TaskWithAction instance.
+ *
+ *
+ * Copyright (C) 2023 Eric Mintz
+ * All Rights Reserved
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * TaskAction's subclasses provide logic that a TaskWithAction runs.
  */
 
-#include "TaskAction.h"
+#ifndef TASKACTION_H_
+#define TASKACTION_H_
 
-#include "TaskWithAction.h"
+#include "Arduino.h"
 
-TaskAction::TaskAction() :
-  containing_task(NULL) {
-}
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 
-TaskAction::~TaskAction() {
-}
+class TaskWithAction;
 
-void TaskAction::delay_millis(uint32_t millis) {
-  containing_task->delay_millis(millis);
-}
+class TaskAction {
 
-void TaskAction::stop(void) {
-  containing_task->stop();
-}
+  // Permits code in the TaskWithAction class to access fields and
+  // functions in this class
+  friend TaskWithAction;
 
-void TaskAction::suspend(void) {
-  containing_task->suspend();
-}
+  TaskWithAction *containing_task;
 
-uint32_t TaskAction::wait_for_notification(uint32_t millis) {
-  return containing_task->wait_for_notification(millis);
-}
+protected:
+  TaskAction(void);
 
-void TaskAction::yield(void) {
-  containing_task->yield();
-}
+  /**
+   * Virtual destructor to ensure that the Action class can support dynamic
+   * casting.
+   */
+  virtual ~TaskAction();
+
+  /**
+   * Delay (pause) the task for the specified number of milliseconds. The task
+   * loop will stop running for the specified time, and resume automatically
+   * afterward. The implementation forwards the request to the containing
+   * TaskWithAction. Implementations should invoke it instead of accessing
+   * the containing TaskWithAction directly.
+   */
+  void delay_millis(uint32_t millis);
+
+  /**
+   * Runs the task logic. Subclasses **must** override and implement this
+   * function. The task logic should either loop indefinitely or call stop()
+   * to exit.
+   */
+  virtual void run() = 0;
+
+  /**
+   * Stops the containing task. The current task is torn down and can no longer
+   * be used. Prefer invoking calling stop() to returning from the task loop.
+   * Note that this method does not return.
+   */
+  void stop(void);
+
+  /**
+   * Suspend this task. The task logic is halted until the application calls
+   * the containing task's resume() function.
+   */
+  void suspend(void);
+
+  /**
+   * Suspend the task until some other task sends a notification or until
+   * the specified delay elapses.
+   *
+   * Parameters
+   *
+   * Name            Contents
+   * --------------- ----------------------------------------------------------
+   * millis_to_wait  Delay in milliseconds, which defaults to the maximum
+   *                 possible value if omitted. If millis_to_wait is zero,
+   *                 the method will return immediately.
+   *
+   * Returns the task's notification count before the method was invoked.
+   * Note that the method sets the notification count to 0, forcing the
+   * task to wait. The implementation forwards the request to the containing
+   * TaskWithAction. Implementations should favor it over accessing the
+   * containing TaskWithAction directly.
+   */
+  uint32_t wait_for_notification(uint32_t millis_to_wait=portMAX_DELAY);
+
+  /**
+   * Yield to any higher priority tasks that are ready to run. Note that
+   * FreeRTOS implements cooperative multitasking, which means that it
+   * cannot forcefully preempt a low priority task. Instead, low
+   * priority tasks must volunteer for preemption. The implementation
+   * forwards the request to the containing TaskWithAction. Implementations
+   * should favor it over accessing the containing TaskWithAction directly.
+   */
+  void yield(void);
+};
+
+
+#endif // TASKACTION_H_
