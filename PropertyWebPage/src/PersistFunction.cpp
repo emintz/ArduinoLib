1554b79ce4f132ca629892f1cb397abd1a45231b
Eric Mintz <16582299+emintz@users.noreply.github.com>
2025-04-17 14:58:43 -0400

    Fix PersistFunction::operator() to overide the pure virtual declaration DataFieldFunction::operator(). This was missed during the deconstification of the DataFieldFunction.

---
+++ PropertyWebPage/src/PersistFunction.cpp
@@ -0,0 +1,40 @@
+/*
+ * PersisFunction.cpp
+ *
+ *  Created on: Apr 15, 2025
+ *      Author: Eric Mintz
+ *
+ * Copyright (c) 2025, Eric Mintz
+ * All Rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "PersistFunction.h"
+#include "ToFlash32Persister.h"
+
+PersistFunction::PersistFunction(
+    Flash32Namespace& eeprom,
+    PersistStatus& errors) :
+        eeprom(eeprom),
+        errors(errors) {
+}
+
+PersistFunction::~PersistFunction() {
+}
+
+bool PersistFunction::operator() (DataFieldConfig& field_config) {
+  const ToFlash32Persister& persister = field_config.get_persister();
+  return persister(field_config, eeprom, errors);
+}
---
+++ PropertyWebPage/src/PersistFunction.h
@@ -0,0 +1,47 @@
+/*
+ * PersisFunction.h
+ *
+ *  Created on: Apr 15, 2025
+ *      Author: Eric Mintz
+ *
+ * DataFieldFunction that persists a property value.
+ *
+ * Copyright (c) 2025, Eric Mintz
+ * All Rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef PERSISTFUNCTION_H_
+#define PERSISTFUNCTION_H_
+
+#include "DataFieldFunction.h"
+
+class Flash32Namespace;
+class PersistStatus;
+
+class PersistFunction : DataFieldFunction {
+  Flash32Namespace& eeprom;
+  PersistStatus& errors;
+
+public:
+  PersistFunction(
+      Flash32Namespace& eeprom,
+      PersistStatus& errors);
+  virtual ~PersistFunction();
+
+  virtual bool operator() (DataFieldConfig& field_config);
+};
+
+#endif /* PERSISTFUNCTION_H_ */
